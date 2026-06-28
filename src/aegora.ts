d42dac88fc7846cc2eea9068c65497a2c8467b46
Matt Williams <mw@logprophet.dev>
2026-06-28 15:52:15 -0600

    feat: add Aegora public entrypoint

---
+++ src/aegora.ts
@@ -0,0 +1,15 @@
+import Elysia from './index'
+
+export * from './index'
+
+/**
+ * Primary Aegora application class.
+ */
+export const Aegora = Elysia
+
+/**
+ * @deprecated Use `Aegora` instead.
+ */
+export { Elysia }
+
+export default Aegora
